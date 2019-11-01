47f1a1331398e606f3ab5e3f967522adb6e7d23b
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-11-01 13:12:24 -0700

    Update main.c

--- lab9_PWM/source/main.c
+++ lab9_PWM/source/main.c
@@ -14,7 +14,7 @@
 
 void set_PWM(double frequency){
     state double current_frequency;
-    if(frequency !- current_frequency){
+    if(frequency != current_frequency){
         if(!frequency){ TCCR3B &= 0x08; }
         else{ TCCR3B |= 0x03; }
         if(frequency < .954){ OCR3A = 0xFFFF;}
