afebe16d88a88b0419e5b8df6c41df84e85037f8
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-11-01 13:07:09 -0700

    Update main.c

--- lab9_PWM/source/main.c
+++ lab9_PWM/source/main.c
@@ -12,6 +12,30 @@
 #include "simAVRHeader.h"
 #endif
 
+void set_PWM(double frequency){
+    state double current_frequency;
+    if(frequency !- current_frequency){
+        if(!frequency){ TCCR3B &= 0x08; }
+        else{ TCCR3B |= 0x03; }
+        if(frequency < .954){ OCR3A = 0xFFFF;}
+        else if(frequency > 31250) { OCR3A = 0x0000; }
+        else { OCR3A = (short)(8000000 / (128* frequency)) -1; }
+        TCNT3 = 0;
+        current_frequency = frequency;
+    }
+}
+
+void PWM_on(){
+    TCCR3A = (1 << COM3A0);
+    TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+    set_PWM(0);
+}
+
+void PWM_off(){
+    TCCR3A = 0x00;
+    TCCR3B = 0x00;
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
 
